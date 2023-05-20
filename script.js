31dde974f017e1179261a07ddbf66b8e038e0736
Ji-yoh <geovannih514@gmail.com>
2023-05-20 00:03:23 -0700

    password character object array created, generate pw pulling single characters

--- script.js
+++ script.js
@@ -3,24 +3,28 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 
-// need to create array for password characters? find a way to generate random string, length input in user prompt. generate 128 character random string consisting of numbers, upper & lowercase letters, and special characters.
+// need to create array for password characters? 
+// find a way to generate random string, length input using user prompt. 
+// generate 128 character random string consisting of numbers, upper & lowercase letters, and special characters.
 
-// declared variables for length and character types
+// created object variable with arrays for all possible password characters
+var passwordCharacters = {
+  uppercase: ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"],
+  lowercase:  ['a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z'],
+  numbers: [0,1,2,3,4,5,6,7,8,9],
+  special: ['!,#,$,%,&,),(,*,+,,,-,.,/,:,;,<,=,>,?,@,[,\,],^,_,`,},{,|,~,'],
+};
 
-// created variable with array for all possible password characters
-var passwordText = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!#$%&)(*+,-./:;<=>?@[\]^_`}{|~ '.split(
-  ''
-);
+// create generatePassword function calling passwordCharacters object array
+// randomize passwordCharacters based on user prompt choices
+// add event listener and user prompts
 
-// create generatePassword function calling passwordText array
 function generatePassword() {
-  var password = '';
-  for (var i = 0; i < passwordText.length; i++) {
-    password = passwordText[Math.floor(Math.random() * passwordText.length)]
-  };
+  var randomPassword = Math.floor(Math.random() * passwordCharacters.uppercase.length);
+  password = passwordCharacters.uppercase[randomPassword];
   
   return password;
-}
+};
 
 function writePassword() {
   var password = generatePassword();
@@ -28,7 +32,7 @@ function writePassword() {
 
   passwordText.value = password;
 
-}
+};
 
 
 
